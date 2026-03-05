7cea91402fbddfe0d8fce58a8f0641dba53c0656
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2026-03-05 21:03:48 +0100

    update(cli_args): better documentation. cmake project version changed to match version in the code

--- src/cli/cli_args.h
+++ src/cli/cli_args.h
@@ -5,9 +5,14 @@
 
 /**
  * @brief Parses arguments provided for the debugger and 
+ *
+ *  To distinguish between debugger and debugee arguments passed through command line arguments a `--` separator is used. 
+ *  All arguments provided after this operator are debugees flags and arguments. By setting `target_argv` to next argument
+ *  after the separator.
+ *
  * @param argc Argument count passed to `main` function
  * @param argv Pointer to arguments array passed to `main` function 
- * @param target_argv Pointer to target arguments variable holding a pointer to target program arguments list
+ * @param target_argv Pointer to debugee arguments list pointer 
  * @returns 0 on success or lack of arguemnts; 1 otherwise;
  */
 int cli_parse_arguments(int argc, char** argv, char*** target_argv);
