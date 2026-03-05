f13c3b41157b9f3bf32180826c225306f359a393
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2026-03-05 13:46:39 +0100

    update(cli_args): documentation added

--- src/cli/cli_args.h
+++ src/cli/cli_args.h
@@ -3,6 +3,13 @@
 #ifndef CLI_ARGS_H
 #define CLI_ARGS_H
 
+/**
+ * @brief Parses arguments provided for the debugger and 
+ * @param argc Argument count passed to `main` function
+ * @param argv Pointer to arguments array passed to `main` function 
+ * @param target_argv Pointer to target arguments variable holding a pointer to target program arguments list
+ * @returns 0 on success or lack of arguemnts; 1 otherwise;
+ */
 int cli_parse_arguments(int argc, char** argv, char*** target_argv);
 
 #endif // CLI_ARGS_H
